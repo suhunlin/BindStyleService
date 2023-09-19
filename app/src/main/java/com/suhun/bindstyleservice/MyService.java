4eceb6bea6e2405ab8ed0573d7d6518bd8cee4e1
suhunlin <lin.suhun@msa.hinet.net>
2023-09-19 16:52:16 +0800

    Remove some code

--- app/src/main/java/com/suhun/bindstyleservice/MyService.java
+++ app/src/main/java/com/suhun/bindstyleservice/MyService.java
@@ -5,12 +5,10 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MyService extends Service {
-    public MyService() {
-    }
+    private String tag = MyService.class.getSimpleName();
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
